5a698ac02698d6aefcda73e59ec84dc48a40cbfb
Yauheni Nikalayeu <yauheni_nikalayeu@epam.com>
2017-11-21 18:21:19 +0300

    Change design

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,7 +17,13 @@ public class Main extends Application {
         SceneSelector.addScene("AUTO_KORREL_SCENE",new Scene(autoKorrelRoot, 300, 275));
         Parent crossKorrelRoot = FXMLLoader.load(getClass().getResource("../crosskorrel.fxml"));
         SceneSelector.addScene("CROSS_KORREL_SCENE",new Scene(crossKorrelRoot, 300, 275));
-        SceneSelector.chooseScene("CROSS_KORREL_SCENE");
+        Parent chooseAutoRoot = FXMLLoader.load(getClass().getResource("../chooseAuto.fxml"));
+        SceneSelector.addScene("CHOOSE_AUTO_SCENE",new Scene(chooseAutoRoot, 300, 275));
+        Parent chooseCrossRoot = FXMLLoader.load(getClass().getResource("../chooseCross.fxml"));
+        SceneSelector.addScene("CHOOSE_CROSS_SCENE",new Scene(chooseCrossRoot, 300, 275));
+        Parent mainRoot = FXMLLoader.load(getClass().getResource("../main.fxml"));
+        SceneSelector.addScene("MAIN_SCENE",new Scene(mainRoot, 300, 275));
+        SceneSelector.chooseScene("CHOOSE_CROSS_SCENE");
     }
 
 
